ab4f39ab3003960454df6113d52e2214f28f4b33
patrick <blockchainbuddha@gmail.com>
2022-02-10 12:43:36 -0800

    add node version

---
+++ src/node/getBoardBase64-node.js
@@ -0,0 +1,124 @@
+const {
+  cols,
+  black,
+  defaultSize,
+  defaultLight,
+  defaultDark,
+  defaultStyle,
+  filePaths,
+} = require("../config/index");
+
+const { promises } = require("fs");
+const { createCanvas, Image } = require("@napi-rs/canvas");
+
+const loadImage = async (imagePath) => {
+  const buffer = await promises.readFile(imagePath);
+  const image = new Image();
+  image.src = buffer;
+  return image;
+};
+
+/**
+ *
+ * @typedef {object} Options
+ * @property {number} [size] Pixel length of desired image
+ * @property {string} [light] Color of light squares
+ * @property {string} [dark] Color of dark squares
+ * @property {"merida"|"alpha"|"cheq"} [style] Desired style of pieces
+ * @property {boolean} [flipped] Whether the board is to be flipped or not
+ */
+/**
+ * Object constructor, initializes options.
+ * @param {Options} [options] Optional options
+ */
+function ChessImageGenerator(options = {}) {
+  this.size = options.size || defaultSize;
+  this.light = options.light || defaultLight;
+  this.dark = options.dark || defaultDark;
+  this.style = options.style || defaultStyle;
+  this.flipped = options.flipped || false;
+}
+
+ChessImageGenerator.prototype = {
+  /**
+   * Generates buffer image based on position
+   * @param {string} boardLayout Pre-computed board layout object
+   * @returns {Promise<Buffer>} Image buffer
+   */
+  async generateDataURL(boardLayout) {
+    const canvas = createCanvas(this.size, this.size);
+    const ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.rect(0, 0, this.size, this.size);
+    ctx.fillStyle = this.light;
+    ctx.fill();
+
+    const row = this.flipped ? (r) => r + 1 : (r) => 7 - r + 1;
+    const col = this.flipped ? (c) => c : (c) => 7 - c;
+
+    for (let i = 0; i < 8; i += 1) {
+      for (let j = 0; j < 8; j += 1) {
+        if ((i + j) % 2 === 0) {
+          ctx.beginPath();
+          ctx.rect(
+            (this.size / 8) * (7 - j + 1) - this.size / 8,
+            (this.size / 8) * i,
+            this.size / 8,
+            this.size / 8
+          );
+          ctx.fillStyle = this.dark;
+          ctx.fill();
+        }
+        const piece = boardLayout[cols[col(j)] + row(i)];
+        if (
+          piece &&
+          piece.type !== "" &&
+          black.includes(piece.type.toLowerCase())
+        ) {
+          const image = `resources/${this.style}/${
+            filePaths[`${piece.color}${piece.type}`]
+          }.png`;
+          const imagePath = `${__dirname}/../${image}`;
+          const imageFile = await loadImage(imagePath);
+          await ctx.drawImage(
+            imageFile,
+            (this.size / 8) * (7 - j + 1) - this.size / 8,
+            (this.size / 8) * i,
+            this.size / 8,
+            this.size / 8
+          );
+        }
+      }
+    }
+    return canvas.toDataURL();
+  },
+};
+
+/**
+ * Loads PGN into chess.js object, and calculates the location pieces
+ * @returns {Promise<Buffer>} Image buffer
+ */
+// outputs base64 data for a jpeg, NOT a png
+const getBoardBase64Node = async (boardLayout, playerColor, options) => {
+  if (!boardLayout) {
+    throw new Error("no PGN passed");
+  }
+  let config = {
+    size: 640,
+    dark: "rgb(181, 137, 98)",
+    light: "rgb(241, 216, 180)",
+    style: "alpha",
+  };
+  if (options) config = options;
+
+  let flipped = false;
+  if (playerColor === "black") flipped = true;
+
+  config.flipped = flipped;
+
+  const imageGenerator = new ChessImageGenerator(config);
+  return imageGenerator.generateDataURL(boardLayout);
+};
+
+module.exports = getBoardBase64Node;
--- tests/main.test.js
+++ tests/main.test.js
@@ -5,6 +5,7 @@ const { Chess } = require("chess.js");
 const { performance } = require("perf_hooks");
 
 const { getBoardLayout, getBoardBase64 } = require("../src/");
+const getBoardBase64Node = require("../src/node/getBoardBase64-node");
 
 const testMoves1 =
   "1. e4 e6 2. d4 d5 3. Nd2 c5 4. Ngf3 Nf6 5. e5 Nfd7 6. c3 Nc6 7. Bd3 Qb6 8. O-O cxd4 9. cxd4 Nxd4 10. Nxd4 Qxd4 11. Nf3 Qb6 12. Qa4 a6 13. Qg4 g6 14. Bg5 Bg7 15. Rfe1 Nc5 16. Qd4 Nd7 17. Qf4 Nc5 18. Qd4 Nd7 19. Qc3 Qc5 20. Qd2 b5 21. Rac1 Qa7 22. Qb4 Bf8 23. Qa5 Qb6 24. Qxb6 Nxb6 25. Rc7 Nd7 26. Rec1 Bc5 27. b4 Bxf2+ 28. Kxf2 O-O 29. Be7 Re8 30. Bd6 Nb6 31. Ng5 Nc4 32. Nxf7 a5 33. Nh6+ Kh8 34. Be7 Rxe7 35. Rxe7 axb4 36. Ng4 Kg8 37. Bb1 d4 38. Kg3 Ra3+ 39. Kh4 Ra8 40. Rf1 1-0";
@@ -12,6 +13,25 @@ const testMoves1 =
 const testCanvas = async (boardLayout, playerColor, customConfig) => {
   var startCanvasTime = performance.now();
   let boardBase64 = await getBoardBase64(
+    boardLayout,
+    playerColor,
+    customConfig
+  );
+  writeFileSync(
+    `${path.join(__dirname, "./")}/test-${playerColor}${
+      customConfig ? "-custom" : ""
+    }.png`,
+    boardBase64.split("base64,")[1],
+    "base64"
+  );
+  var endCanvasTime = performance.now();
+  console.log(
+    `Canvas: ${((endCanvasTime - startCanvasTime) / 1000).toFixed(3)} seconds`
+  );
+};
+const testCanvasNode = async (boardLayout, playerColor, customConfig) => {
+  var startCanvasTime = performance.now();
+  let boardBase64 = await getBoardBase64Node(
     boardLayout,
     playerColor,
     customConfig
@@ -44,6 +64,10 @@ const test = async (moves, playerColor, customConfig) => {
   const boardLayout = testBoard(moves);
   await testCanvas(boardLayout, playerColor, customConfig);
 };
+const testNode = async (moves, playerColor, customConfig) => {
+  const boardLayout = testBoard(moves);
+  await testCanvasNode(boardLayout, playerColor, customConfig);
+};
 
 const main = async () => {
   await test(testMoves1, "white");
@@ -56,7 +80,7 @@ const main = async () => {
     style: "cheq",
   };
   await test(testMoves1, "white", customConfig);
-
+  await testNode(testMoves1, "white", customConfig);
   console.log("====== done ======");
 };
 
