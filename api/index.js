f0a65967e5c9490d6bf9471fe5d52ffe4c56988d
patrick <blockchainbuddha@gmail.com>
2022-02-10 17:26:38 -0800

    cleanup

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const { getBoardLayout } = require("../src/getBoardLayout");
+const getBoardLayout = require("../src/getBoardLayout/src/getBoardLayout");
 const getBoardBase64Node = require("../src/node/getBoardBase64-node");
 
 module.exports = async (req, res) => {
